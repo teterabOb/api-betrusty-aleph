610eb29bfa5243b5ae75cd0edf3b0caaf266169a
Gilberts Ahumada <gilbertsahumada@Gilbertss-MacBook-Pro.local>
2024-08-14 19:19:15 -0300

    new workflow for auth done

--- routes/auth/ml/mlAuth.ts
+++ routes/auth/ml/mlAuth.ts
@@ -7,11 +7,13 @@ const router = express.Router();
 
 const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = GetGitHubEnv();
 
+// Punto de entrada para generar el Token de autenticación
 router.get("/", async (_req, res) => {
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
   res.redirect(githubAuthUrl);
 });
 
+// Callback de la autenticación que consultará Github
 router.get("/callback", async (req: Request, res: Response) => {
   const { code } = req.query;
 
@@ -35,14 +37,31 @@ router.get("/callback", async (req: Request, res: Response) => {
 
     const { access_token } = tokenResponse.data;
 
-    const userResponse = await axios.get("https://api.github.com/user", { headers: { Authorization: `token ${access_token}` } });
+    //const userResponse = await axios.get("https://api.github.com/user", { headers: { Authorization: `token ${access_token}` } });
 
-    const userData = userResponse.data;
-    return res.json(userData);
+    //const userData = userResponse.data;
+    return res.redirect(`/user-info?access_token=${access_token}`);
   } catch (error) {
     return res.status(500).send({ error: error });
   }
 });
+
+// Obtiene la información del Usuario
+router.get("/user-info", async (req: Request, res: Response) => { 
+    const {access_token} = req.query;
+
+    if(!access_token){
+        return res.status(400).send("Access token not found");
+    }
+
+    try {
+        const userResponse = await axios.get("https://api.github.com/user", { headers: { Authorization: `token ${access_token}` } });
+        const userData = userResponse.data;
+        return res.json(userData);
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+})
 
 export default router;
 

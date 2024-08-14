4a3537c90a9c2f99bd228940f70df15d90f7c1e0
Gilberts Ahumada <gilbertsahumada@Gilbertss-MacBook-Pro.local>
2024-08-14 13:08:44 -0300

    previous deploy

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,7 @@
 import server from './server'
 import { Request, Response, NextFunction} from "express"
 import transactionRouter from './routes/transactions'
-import dotenv from 'dotenv'
 
-dotenv.config()
 
 const PORT = 3003
 
@@ -16,19 +14,18 @@ server.get('/', (req, res) => {
  })
     
 server.use('/api/transactions', transactionRouter)
-server.use('/api/auth/github/', transactionRouter)
-server.use('/api/auth/worldid/', transactionRouter)
-server.use('/api/auth/ml/', transactionRouter)
-
-
-server.listen(PORT, () => {
-    console.log(`Server running on port : ${PORT}!`)
-})
+//server.use('/api/auth/github/', transactionRouter)
+//server.use('/api/auth/worldid/', transactionRouter)
+//server.use('/api/auth/ml/', transactionRouter)
 
 //Middleware de manejo de errores
 server.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
     console.error(err)
     res.status(500).send('Something went wrong!')
+ })
+
+server.listen(PORT, () => {
+    console.log(`Server running on port : ${PORT}!`)
 })
 
 module.exports = server

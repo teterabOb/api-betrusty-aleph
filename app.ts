301d74728fed9807abedc40a2635306b5f1cadfe
Gilberts Ahumada <gilbertsahumada@Gilbertss-MacBook-Pro.local>
2024-08-16 23:15:21 -0300

    fixing errors

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 //import helmet from 'helmet';
-//import cors from "cors"
+import cors from "cors"
 import routes from './routes';
 
 const app = express();
+app.use(cors({ origin: "*" }))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true, }))
 app.use('/', routes)

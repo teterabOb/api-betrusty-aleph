d6225f39b3f8e71047d582070e592f37cf8689bf
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-08-20 22:14:00 -0300

    removing token

--- routes/index.ts
+++ routes/index.ts
@@ -6,7 +6,7 @@ import userTable from "./tables/userTable"
 const routes = Router();
 
 routes.use("/github", github)
-routes.use("/world-id", worldId)
+routes.use("/worldid", worldId)
 routes.use("/user-table", userTable)
 routes.get('/', (req, res) => res.json(('Express + TypeScript Server on Vercel')));
 

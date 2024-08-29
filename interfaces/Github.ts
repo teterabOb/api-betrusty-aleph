a7a2f728ad0320ca66974747d6f497368f5027df
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-08-29 17:23:35 -0300

    object data parse to string added

--- interfaces/Github.ts
+++ interfaces/Github.ts
@@ -1,10 +1,10 @@
 export interface Github {
     reputation_level?: string,
     github_login: string;
-    github_public_repos: number;
-    github_public_gists: number;
-    github_followers: number;
-    github_following: number;
+    github_public_repos: number | string;
+    github_public_gists: number | string;
+    github_followers: number | string;
+    github_following: number | string;
     github_created_at: string;  
     github_collaborators: string;
 }
--- interfaces/MercadoLibre.ts
+++ interfaces/MercadoLibre.ts
@@ -6,14 +6,14 @@ export interface MercadoLibre {
     mercado_libre_identification_number: string;
     mercado_libre_identification_type: string;
     mercado_libre_seller_experience: string;
-    mercado_libre_seller_reputation_transactions_total: number;
-    mercado_libre_seller_reputation_transactions_completed: number;
-    mercado_libre_seller_reputation_transactions_canceled: number;
-    mercado_libre_seller_reputation_ratings_positive: number;
-    mercado_libre_seller_reputation_ratings_negative: number;
-    mercado_libre_seller_reputation_ratings_neutral: number;
-    mercado_libre_buyer_reputation_canceled_transactions: number;
-    mercado_libre_buyer_reputation_transactions_total: number;
-    mercado_libre_buyer_reputation_transactions_completed: number;
+    mercado_libre_seller_reputation_transactions_total: number | string;
+    mercado_libre_seller_reputation_transactions_completed: number | string;
+    mercado_libre_seller_reputation_transactions_canceled: number | string;
+    mercado_libre_seller_reputation_ratings_positive: number | string;
+    mercado_libre_seller_reputation_ratings_negative: number | string;
+    mercado_libre_seller_reputation_ratings_neutral: number | string;
+    mercado_libre_buyer_reputation_canceled_transactions: number | string;
+    mercado_libre_buyer_reputation_transactions_total: number | string;
+    mercado_libre_buyer_reputation_transactions_completed: number | string;
     mercado_libre_nickname: string;
 }
